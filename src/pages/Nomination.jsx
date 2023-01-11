569a472e794820837e392486365ac0b2dba7d23c
Tima <timas3811@gmail.com>
2023-01-11 09:59:41 +0300

    search and filter

--- src/pages/Nomination.jsx
+++ src/pages/Nomination.jsx
@@ -1,43 +1,40 @@
-
-import '../styles/main.css'
 import Header from '../components/header/Header';
+import '../styles/main.css'
 import Nominate from '../components/Nominate/Nomnate';
 import {nomination} from '../components/help-elements/NominationList';
 import { useState } from 'react';
 
-// const [value, setValue] = useState()
-// const filterNomination = nomination.filter(nomination =>{
-//     return nomination.title.toLocaleLowerCase().includes(value.toLowerCase())
-// })
 
-// function useState(){
-//     const [value, setValue] = useState() 
-//     const filterNomination = nomination.filter(nomination =>{
-//         return nomination.title.toLocaleLowerCase().includes(value.toLowerCase())
-//     })
-// }
 const Nomination = () =>{
+    const [value, setValue] = useState('')
+
+    const filterNomination = nomination.filter(nomination =>{
+        return nomination.title.toLowerCase().includes(value.toLowerCase())
+    })
     return(
+        <div>
 
-    <div>
-        <Header/>
-            <main className="section">
-                <div className="container">
-                <h2 className="title-1">Номинации:</h2>
-                <ul className="projects">
+<Header/>
 
-            {filterNomination.map((nomination, index) =>{
-                    return(
-                        <Nominate
-                        key={index}
-                        title={nomination.title} img={nomination.img}/>
-                    )
+<main className="section">
+<div className="container">
+    <h2 className="title-1">Номинации:</h2>
+    <ul className="projects">
 
-                    
-                })}
+        {filterNomination.map((nomination, index) =>{
+            return(
+                <Nominate key={index} title={nomination.title} img={nomination.img}
+                    />
+            )
+        })}
 
-                </ul>
-                <div class="form">
+ 
+        
+    </ul>
+    
+</div>
+</main>
+<div class="form">
                         <form class="search__form">
                                 <input type="text" placeholder="Введите название чего то там" 
                                 class="search__input"
@@ -45,14 +42,8 @@ const Nomination = () =>{
                                 <img src='#' alt="" class="search-icon"/>
                         </form>
                     </div>
-                </div>
-            </main>
-
-    </div>
+        </div>
        
             );
-            
 }
-
-
 export default Nomination;
